4efc7a3dfb2ed3e2b7cbb419cad87f674cee4f1c
Frank Schmitz <frank.schmitz@rewe-digital.com>
2020-03-04 14:06:05 +0100

    Updated to imagio-ffmpeg

--- README.md
+++ README.md
@@ -4,20 +4,20 @@ Object detection using deep learning with Yolo, OpenCV and Python via Real Time 
 
 Recognized objects are stored in date seperated in folders per class for further training or face recognition.
 
-OpenCV `dnn` module supports running inference on pre-trained deep learning models from popular frameworks like Caffe, Torch and TensorFlow. 
+OpenCV `dnn` module supports running inference on pre-trained deep learning models from popular frameworks like Caffe, Torch and TensorFlow.
 
 When it comes to object detection, popular detection frameworks are
  * YOLO
  * SSD
  * Faster R-CNN
- 
- Support for running YOLO/DarkNet has been added to OpenCV dnn module recently. 
- 
+
+ Support for running YOLO/DarkNet has been added to OpenCV dnn module recently.
+
  ## Dependencies
   * opencv
   * numpy
   * imageio ( version 2.4.1 )
-  
+
 `pip install numpy opencv-python imageio`
 `sudo pip3 install imageio==2.4.1`
 
@@ -25,23 +25,23 @@ When it comes to object detection, popular detection frameworks are
 **Note: Python 2.x is not supported**
 
  ## YOLO (You Only Look Once)
- 
+
  Download the pre-trained YOLO v3 weights file from this [link](https://pjreddie.com/media/files/yolov3.weights) or for tiny weights for slower machines [link](https://pjreddie.com/media/files/yolov3-tiny.weights) and place it in the current directory or you can directly download to the current directory in terminal using
- 
+
  `$ wget https://pjreddie.com/media/files/yolov3.weights`
- 
+
  `$ wget https://pjreddie.com/media/files/yolov3-tiny.weights`
- 
+
  Provided all the files are in the current directory, below command will apply object detection on the input image `dog.jpg`.
- 
- `$ python yolo_opencv.py --input sampledata/commuters.mp4 --config cfg/yolov3.cfg --weights yolov3.weights --classes cfg/yolov3.txt`
- 
+
+ `$ python yolo_opencv.py --input sampledata/commuters.mp4 --config cfg/yolov3-tiny.cfg --weights yolov3-tiny.weights --classes cfg/yolov3.txt`
+
  For RTSP simply put the RTSP URL as --input
- 
+
   `$ python yolo_opencv.py --input rtsp://xxxxx:1935/live/sys.stream --framestart 100 --framelimit 100 --config cfg/yolov3-tiny.cfg --weights yolov3-tiny.weights --classes cfg/yolov3.txt`
 
- **Arguments** 
- 
+ **Arguments**
+
  | parameter | type    | description                                      |
  | --------- | ------- | ------------------------------------------------ |
  | `input`     | String  | /path/to/input/stream |
@@ -54,13 +54,13 @@ When it comes to object detection, popular detection frameworks are
  | `classes`  | String | /path/to/classes/file |
  | `invertcolor` | Boolean  | in case of BGR streams |
  | `fpsthrottle` | Int  | in case of slower machines to keep up with a stream  |
- 
+
  ### sample output :
  ![](object-detection.png)
- 
+
 Checkout the object detection implementation available in [cvlib](http:cvlib.net) which enables detecting common objects in the context through a single function call `detect_common_objects()`.
- 
+
  ## Credits
  This project is based on [Arun Ponnusamy's Object Detection OpenCV](https://github.com/arunponnusamy/object-detection-opencv)
- 
+
  Sample video footage from [Videvo - Free Stock Video Footage](https://www.videvo.net/video/people-crossing-road-in-hong-kong-cbd/8162/)
--- yolo_opencv.py
+++ yolo_opencv.py
@@ -9,10 +9,8 @@ import os.path
 import cv2
 import argparse
 import numpy as np
-import imageio
+import imageio_ffmpeg as imageio
 import datetime
-
-imageio.plugins.ffmpeg.download()
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--input', required=False,
@@ -32,7 +30,7 @@ ap.add_argument('-w', '--weights', required=False,
 ap.add_argument('-cl', '--classes', required=False,
                 help = 'path to text file containing class names',  default = 'cfg/yolov3.txt')
 ap.add_argument('-ic', '--invertcolor', required=False,
-                help = 'invert RGB 2 BGR',  default = 'false')
+                help = 'invert RGB 2 BGR',  default = 'False')
 ap.add_argument('-fpt', '--fpsthrottle', required=False,
                 help = 'skips (int) x frames in order to catch up with the stream for slow machines 1 = no throttle',  default = 1)
 args = ap.parse_args()
@@ -46,9 +44,9 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def get_output_layers(net):
-    
+
     layer_names = net.getLayerNames()
-    
+
     output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
     return output_layers
@@ -58,14 +56,14 @@ def save_bounded_image(image, class_id, confidence, x, y, x_plus_w, y_plus_h):
     dirname = os.path.join(args.outputdir, label, datetime.datetime.now().strftime('%Y-%m-%d'))
     if not os.path.exists(dirname):
         os.makedirs(dirname)
-    
+
     filename = label + '_' + datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S_%f') + '_conf' + "{:.2f}".format(confidence) + '.jpg'
     print ('Saving bounding box:' + filename)
     roi = image[y:y_plus_h, x:x_plus_w]
     if roi.any():
         if str2bool(args.invertcolor) == False:
             roi = cv2.cvtColor(roi, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(os.path.join(dirname, filename), roi)  
+        cv2.imwrite(os.path.join(dirname, filename), roi)
 
 def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     label = str(classes[class_id])
@@ -75,25 +73,25 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 3)
 
 def detect(image):
-        
+
     Width = image.shape[1]
     Height = image.shape[0]
     scale = 0.00392
-    
+
     net = cv2.dnn.readNet(args.weights, args.config)
-    
+
     blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
-    
+
     net.setInput(blob)
-    
+
     outs = net.forward(get_output_layers(net))
-    
+
     class_ids = []
     confidences = []
     boxes = []
     conf_threshold = 0.5
     nms_threshold = 0.4
-    
+
     for out in outs:
         for detection in out:
             scores = detection[5:]
@@ -109,10 +107,10 @@ def detect(image):
                 class_ids.append(class_id)
                 confidences.append(float(confidence))
                 boxes.append([x, y, w, h])
-    
-    
+
+
     indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-    
+
     orgImage = image.copy()
     for i in indices:
         i = i[0]
@@ -129,20 +127,25 @@ def detect(image):
 
 def processvideo(file):
     cap = cv2.VideoCapture(file)
-    writer = imageio.get_writer(args.outputfile)
+
+    writer = imageio.write_frames(args.outputfile, (int(cap.get(3)), int(cap.get(4))))
+    writer.send(None)
     frame_counter = 0
     while(cap.isOpened()):
         frame_counter = frame_counter + 1
         ret, frame = cap.read()
         print('Detecting objects in frame ' + str(frame_counter))
-        if not frame is None:
-            image = detect(frame)
-            writer.append_data(frame)
+        if ret==True:
+            if not frame is None:
+                image = detect(frame)
+                writer.send(frame)
+            else:
+                print('Frame error in frame ' + str(frame_counter))
         else:
-            print('Frame error in frame ' + str(frame_counter))
+            break
     cap.release()
     writer.close()
-    
+
 # Doing some Object Detection on a video
 classes = None
 
@@ -151,22 +154,24 @@ with open(args.classes, 'r') as f:
 COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
 if args.input.startswith('rtsp'):
-    if int(args.framelimit) > 0:
-        writer = imageio.get_writer(args.outputfile)
+
     cap = cv2.VideoCapture(args.input)
+    if int(args.framelimit) > 0:
+        writer = imageio.write_frames(args.outputfile, (int(cap.get(3)), int(cap.get(4))))
+        writer.send(None)
     frame_counter = 0
     while(True):
         if int(args.framelimit) > 0 and frame_counter > int(args.framestart) + int(args.framelimit):
             writer.close()
             break
-        
+
         if frame_counter % int(args.fpsthrottle) ==0:
             ret, frame = cap.read()
             if ret and frame_counter >= int(args.framestart):
                 print('Detecting objects in frame ' + str(frame_counter))
                 frame = detect(frame)
                 if int(args.framelimit) > 0:
-                    writer.append_data(frame)
+                    writer.send(frame)
             else:
                 print('Skipping frame ' + str(frame_counter))
         else:
@@ -180,8 +185,4 @@ else:
                 print('Processing video ' + os.path.join(dirpath, filename))
                 processvideo(os.path.join(dirpath, filename))
     else:
-        reader = imageio.get_reader(args.input)
-        processvideo(reader)
-        
-
-
+        processvideo(os.path.join(args.input))
